44ba6b999221b90dd97095e369268059c1525d96
Erick Mauricio <102829461+Erick03072003@users.noreply.github.com>
2023-09-25 19:32:29 -0500

    Update Authentication.cs

--- Moana/Services/Authentication.cs
+++ Moana/Services/Authentication.cs
@@ -7,8 +7,8 @@ namespace Moana
         private readonly Supabase.Client _supabase;
         public AuthenticationService()
         {
-            var url = "https://aedefiwrqctkhakwuhqb.supabase.co/";
-            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlZGVmaXdycWN0a2hha3d1aHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MTQ4NTYsImV4cCI6MjAxMTE5MDg1Nn0.-Irt1RDGROoKmJyrNdcCUNiIuUMoZWcXP1QRDpPI0sM";
+            var url = "xd";
+            var key = "xd";
             _supabase = new Supabase.Client(url, key);
         }
         public async Task<bool> Authenticate(string email, string password)
