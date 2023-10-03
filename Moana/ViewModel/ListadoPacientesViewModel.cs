b171a91cdd3d1049f6d644c6122701896e8f54ce
Erick Mauricio <coartac.erick.mamani@gmail.com>
2023-10-03 10:18:55 -0500

    add resheshview

--- Moana/ViewModel/ListadoPacientesViewModel.cs
+++ Moana/ViewModel/ListadoPacientesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Moana.Models;
 using Supabase;
@@ -11,13 +13,16 @@ namespace Moana.View
 
         private ObservableCollection<User> patients;
         private User selectedPatient;
+        private bool isRefreshing;
 
         public ListadoPacientesViewModel()
         {
             _supabaseClient = MauiProgram.CreateMauiApp().Services.GetRequiredService<Client>();
+
             LoadPatients();
             AddPatientCommand = new Command(OnAddPatientClicked);
             ShowPatientDetailsCommand = new Command(OnShowPatientDetails);
+            RefreshCommand = new Command(async () => await OnRefresh());
         }
 
         public ObservableCollection<User> Patients
@@ -27,6 +32,38 @@ namespace Moana.View
             {
                 patients = value;
                 OnPropertyChanged();
+            }
+        }
+
+        public ICommand RefreshCommand { get; private set; }
+
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set
+            {
+                isRefreshing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async Task OnRefresh()
+        {
+            IsRefreshing = true;
+
+            await ReloadPatients();
+
+            IsRefreshing = false;
+        }
+
+        private async Task ReloadPatients()
+        {
+            var userService = new UserService(_supabaseClient);
+            var patientsList = await userService.GetPatients();
+            Patients.Clear();
+            foreach (var patient in patientsList)
+            {
+                Patients.Add(patient); 
             }
         }
 
@@ -45,14 +82,14 @@ namespace Moana.View
 
         private async void LoadPatients()
         {
-            var userService = new UserService(_supabaseClient); 
+            var userService = new UserService(_supabaseClient);
             var patientsList = await userService.GetPatients();
             Patients = new ObservableCollection<User>(patientsList);
         }
 
         private void OnAddPatientClicked()
         {
-            var newPatient = new User { Name = "Nuevo Paciente" }; 
+            var newPatient = new User { Name = "Nuevo Paciente" };
             Patients.Add(newPatient);
         }
 
